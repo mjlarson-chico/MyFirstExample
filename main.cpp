cb49d61fbef45a1c8778e0d91e1de62252522659
Michael Larson <mlarson1@mail.csuchico.edu>
2020-01-29 11:44:50 -0800

    blah

--- main.cpp
+++ main.cpp
@@ -5,7 +5,12 @@ using std::cout;
 
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
+    int input1 = 0;
+    int input2 = 0;
+    
+    cout << "Hi, please enter two numbers: ";
+    cin >> input1 >> input2;
+    cout << "The sum of your numbers is: " << input1 + input2;
 
     return 0;
 }
